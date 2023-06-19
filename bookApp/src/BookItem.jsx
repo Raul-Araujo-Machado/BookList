b71140d990358d7d10730d959d202cd8c5dc0e1e
Raul Araujo Machado <raularaujomachado@gmail.com>
2023-06-19 16:33:55 -0300

    add first css

--- bookApp/src/BookItem.jsx
+++ bookApp/src/BookItem.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import './BookItem.css';
 
 const ItemListaLivro = ({ book }) => {
     const [expandido, setExpandido] = useState(false);
@@ -8,10 +9,12 @@ const ItemListaLivro = ({ book }) => {
     };
 
     return (
-        <div onClick={handleClick} style={{ cursor: 'pointer' }}>
-            <span>{book.id}</span>
-            <h3>{book.title}</h3>
-            {expandido && <p>Autor: {book.author}</p>}
+        <div className='book-item' onClick={handleClick} style={{ cursor: 'pointer' }}>
+            <div><span>{book.id}</span></div>
+            <div>
+                <h3>{book.title}</h3>
+                {expandido && <p>Autor: {book.author}</p>}
+            </div>
         </div>
   );
 };
